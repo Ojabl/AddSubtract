a1424674409f2ae4af8c74de9e9275ca019dde2e
osajab10@gmail.com <osajab10@gmail.com>
2022-09-18 15:50:08 +0200

    Add project files.

---
+++ AddSubtract/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace AddSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddSubtract a = new AddSubtract() { Value = 5 }
+            .Add(5)
+            .Subtract(3)
+            .Add(9)
+            .Subtract(12);
+            Console.WriteLine($"Result: {a.Value}");
+        }
+    }
+}
